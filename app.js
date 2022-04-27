4a6c5d9ce602af497ec480c2136b4a435a0e4678
AleMilos <alessandro.milos@hotmail.it>
2022-04-27 20:46:31 +0200

    fixed 650px size cards and added jquery to html (conditionals on screen size for last card and attribute)

---
+++ app.js
@@ -0,0 +1,34 @@
+// If the max width is less than 650 px --> remove .last-card and .last-attributes. 
+
+
+
+function resize() {
+    if ( $(window).width() < 650) {     
+        $("#toRemove1").removeClass("last-card");
+        $("#toRemove2").toggleClass("last-attributes");
+    }
+    else {
+        $("#toRemove1").addClass("last-card");
+        $("#toRemove2").addClass("last-attributes");
+    }
+}
+$(window).on("resize", resize);
+resize(); // call once initially
+
+
+function removeAt650(){
+    var lastCard = document.getElementById("toRemove1");
+    var lastAttribute = document.getElementById("toRemove2");
+    lastCard.classList.remove("last-card");
+    lastAttribute.classList.remove("last-attributes");
+    }
+
+function addAt650(){
+    var lastCard = document.getElementById("toRemove1");
+    var lastAttribute = document.getElementById("toRemove2");
+    lastCard.classList.add("last-card");
+    lastAttribute.classList.add("last-attributes");
+    }
+
+
+   

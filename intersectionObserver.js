65bdc75650fb1f8fd4c38d02e97430f2f942cda1
AleMilos <alessandro.milos@hotmail.it>
2022-05-24 22:49:12 +0200

    tried to implement feature for smaller than 650 px

---
+++ intersectionObserver.js
@@ -0,0 +1,51 @@
+const bubbles = document.querySelectorAll(".step-bubble");
+const titles = document.querySelectorAll(".step-title");
+
+const observer = new IntersectionObserver (entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show-step-bubble", entry.isIntersecting);
+        entry.target.getElementsByClassName('bubble-counter')[0].classList.toggle("show-bubble-counter", entry.isIntersecting);
+        
+        
+        //if(entry.isIntersecting) observer.unobserve(entry.target) NEL CASO IN CUI VOGLIO 1 sola animation
+
+    })
+
+},
+{
+    threshold: 1
+})
+
+const observerTitles = new IntersectionObserver (entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("step-title-less650", entry.isIntersecting)
+        //if(entry.isIntersecting) observer.unobserve(entry.target) NEL CASO IN CUI VOGLIO 1 sola animation
+
+    })
+
+},
+{
+    threshold: 1
+})
+
+
+bubbles.forEach(bubble => {
+    observer.observe(bubble)
+})
+
+const mq = window.matchMedia( "(max-width:40.625em)" );
+
+if (!mq.matches) {
+    console.log("test");
+      
+} else {
+    titles.forEach(title => {
+        observerTitles.observe(title)
+    })
+}
+/*
+    bubbles.forEach(bubble => {
+        observer.observe(bubble)
+    })
+*/
+

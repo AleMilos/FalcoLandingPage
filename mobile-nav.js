90e1bcf3937b7831e206d2d5efce46b126bee06c
AleMilos <alessandro.milos@hotmail.it>
2022-05-30 18:14:26 +0200

    fixed the window on button pressing!

--- mobile-nav.js
+++ mobile-nav.js
@@ -1,10 +1,11 @@
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.getElementsByClassName(".header");
+document.addEventListener("DOMContentLoaded", () => {
+    const btnNavEl = document.querySelector(".btn-mobile-nav");
+    const headerEl = document.getElementsByClassName('header')[0];
 
+    console.log(btnNavEl)
+    console.log(headerEl)
+    btnNavEl.addEventListener("click", function(){
+        headerEl.classList.toggle("nav-open");
 
-console.log(btnNavEl)
-console.log(headerEl)
-
-btnNavEl.addEventListener("click", function(){
-    headerEl.classList.toggle("nav-open");
-});
+    })
+})

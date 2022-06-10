10cbaba614f6858dc8e4d55afa7261f9b1fe7733
AleMilos <alessandro.milos@hotmail.it>
2022-06-10 21:06:20 +0200

    Moved js file to end of the body. Implemented smooth scrolling and fixed the remaining window when clicking on it.

--- mobile-nav.js
+++ mobile-nav.js
@@ -1,11 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const btnNavEl = document.querySelector(".btn-mobile-nav");
-    const headerEl = document.getElementsByClassName('header')[0];
+const headerEl = document.querySelector(".header")
+const btnNavEl = document.querySelector(".btn-mobile-nav");
 
-    console.log(btnNavEl)
-    console.log(headerEl)
-    btnNavEl.addEventListener("click", function(){
-        headerEl.classList.toggle("nav-open");
+// Toggle the window
+btnNavEl.addEventListener("click", function(){
+    headerEl.classList.toggle("nav-open");
+})
 
+const allLinks = document.querySelectorAll("a:link")
+
+allLinks.forEach(function (link) {
+    link.addEventListener('click', function(e){
+        e.preventDefault();
+        const href = link.getAttribute("href");
+        
+        if (href === "#") window.scrollTo({
+            top: 0,
+            behavior: "smooth"     
+        })
+
+        if (href !== "#" && href.startsWith('#')){
+            const sectionEl = document.querySelector(href)
+            sectionEl.scrollIntoView({behavior: "smooth"});
+        }
+
+        if(link.classList.contains('main-nav-link')) {
+            headerEl.classList.toggle("nav-open");
+        }
     })
 })
